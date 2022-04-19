8ad07599616560538c2587ef4834ec71c67f0c57
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-04-19 07:35:23 -0700

    Banner section design

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-
-import banner1 from '../../../images/banner/banner1.jpg';
-import banner2 from '../../../images/banner/banner2.jpg';
-import banner3 from '../../../images/banner/banner3.jpg';
+import banner1 from '../../../images/banner/banner1.jpg'
+import banner2 from '../../../images/banner/banner2.jpg'
+import './Banner.css'
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -15,39 +14,25 @@ const Banner = () => {
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100  custom-img-size"
                     src={banner1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h1>It is even better than an expensive cookery book</h1>
+                    <p>There's definitely a time and a place for slow, intricate, patient dinners but that time is not now. Let's keep this quick and easy!</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="d-block w-100  custom-img-size"
                     src={banner2}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner3}
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <h1>Enjoy your food</h1>
+                    <p>We collect fresh and Authentic food from remote areas of different country districts and prepare it in the organic process to ensure quality.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
