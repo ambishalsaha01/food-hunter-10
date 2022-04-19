8b2d1fa29bc872603cfd7728fc0f432777f40286
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-04-18 22:49:37 -0700

    Set All Project Component

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <h2>this is header</h2>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
