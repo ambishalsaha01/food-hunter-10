16f8a4175e8d4af3fd87516aa75638f7bacb3921
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-04-19 09:54:37 -0700

    initialize all components

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import img from '../../../images/404.jpg';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2 className='text-primary text-center'>Please Go to the right URL</h2>
-            <img className='w-100' src={img} alt="" />
+        <div className='notfound-container'>
+            <h2 className='text-warning mt-5 text-center'>Please Go to the right URL</h2>
+            <img src={img} alt="" />
         </div>
     );
 };
